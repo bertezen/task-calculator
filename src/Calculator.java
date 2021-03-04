e894e01c8845c2859d4a1e5b4fb42a25b678d79f
Dmitriy Pronin <bertezen@mail.ru>
2021-03-04 19:06:48 +0700

    final v1.2

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,23 +11,26 @@ public class Calculator {
 
     public static void main(String[] args) throws InvalidNumberException, IOException {
         String[] mass;
-        int a, b, res;
+        int[] a;
+        int res;
 
         while (true) {
             System.out.println("Input:");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             mass = br.readLine().toUpperCase().split(" ");
+            try {
+                a = ConversionToInt.convers(mass[0], mass[2]);
+                if (a[0] > 10|| a[1] > 10) {
+                    throw new InvalidNumberException("Variables cannot be more than 10");
+                }
 
-            a = ConversionToInt.convers(mass[0]);
-            b = ConversionToInt.convers(mass[2]);
-
-            if (a > 10 || b > 10) {
-                throw new InvalidNumberException("Variables cannot be more than 10");
+                System.out.println("Output:");
+                res = new CalculationNumeral(a[0], a[1]).select(mass[1]);
+                printResult(res, ConversionToInt.roman);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Incorrect operator");
+                break;
             }
-
-            System.out.println("Output:");
-            res = new CalculationNumeral(a, b).select(mass[1]);
-            printResult(res, ConversionToInt.roman);
         }
     }
 
--- src/numbers/ConversionToInt.java
+++ src/numbers/ConversionToInt.java
@@ -3,15 +3,17 @@ package numbers;
 public class ConversionToInt {
     static public boolean roman = false;
 
-    public static int convers(String mass) {
+    public static int[] convers(String mass, String mass1) {
         char[] c = mass.toCharArray();
-        int a = 0;
+        int a[] = new int[2];
         int pos = (int) c[0];
         if (pos > 48 && pos < 58) {
-            a = Integer.parseInt(mass);
+            a[0] = Integer.parseInt(mass);
+            a[1] = Integer.parseInt(mass1);
             roman = false;
         } else if (pos == 73 || pos == 86 || pos == 88) {
-            a = RomanNumeral.valueOf(mass).getI();
+            a[0] = RomanNumeral.valueOf(mass).getI();
+            a[1] = RomanNumeral.valueOf(mass1).getI();
             roman = true;
         }
         return a;
