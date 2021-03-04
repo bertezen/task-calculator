9b23d9e722c963c5096ec19b5d24ae87106cf0f3
Dmitriy Pronin <bertezen@mail.ru>
2021-03-04 12:56:25 +0700

    add print arab/rome

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,7 +9,8 @@ public class Calculator {
 
     public static void main(String[] args) throws IOException {
         String[] mass;
-        int a = 0, b = 0, res;
+        int a = 0, b = 0, pos, res;
+        boolean roman = false;
 
 
         while (true) {
@@ -19,28 +20,34 @@ public class Calculator {
             mass = str.split(" ");
 
             char[] c = mass[0].toCharArray();
-            res = (int) c[0];
+            pos = (int) c[0];
 
-            if (res > 48 && res < 58) {
+            if (pos > 48 && pos < 58) {
                 a = Integer.parseInt(mass[0]);
                 b = Integer.parseInt(mass[2]);
-            } else if (res == 73 || res == 86 || res == 88) {
+                roman = false;
+            } else if (pos == 73 || pos == 86 || pos == 88) {
                 a = RomanNumeral.valueOf(mass[0]).getI();
                 b = RomanNumeral.valueOf(mass[2]).getI();
+                roman = true;
             }
 
             switch (mass[1]) {
                 case "+":
-                    System.out.println(new ArabicNumeral(a, b).sum());
+                    res = new ArabicNumeral(a, b).sum();
+                    System.out.println(roman ? RomanNumeral.toArabicNumeral(res) : res);
                     break;
                 case "-":
-                    System.out.println(new ArabicNumeral(a, b).subtract());
+                    res = new ArabicNumeral(a, b).subtract();
+                    System.out.println(roman ? RomanNumeral.toArabicNumeral(res) : res);
                     break;
                 case "*":
-                    System.out.println(new ArabicNumeral(a, b).multi());
+                    res = new ArabicNumeral(a, b).multi();
+                    System.out.println(roman ? RomanNumeral.toArabicNumeral(res) : res);
                     break;
                 case "/":
-                    System.out.println(new ArabicNumeral(a, b).division());
+                    res = new ArabicNumeral(a, b).division();
+                    System.out.println(roman ? RomanNumeral.toArabicNumeral(res) : res);
                     break;
 
             }
--- src/numbers/RomanNumeral.java
+++ src/numbers/RomanNumeral.java
@@ -1,11 +1,11 @@
 package numbers;
 
 public enum RomanNumeral {
-    I("I", 1), II("II", 2), III("III", 3), IV("IV", 4), V("V", 5), VI("VI", 6), VII("VII", 7), VIII("VIII", 8), XI("IX", 9), X("X", 10);
+    I("I", 1), II("II", 2), III("III", 3), IV("IV", 4), V("V", 5), VI("VI", 6), VII("VII", 7), VIII("VIII", 8), IX("IX", 9), X("X", 10);
     private int i;
     private String s;
 
-     RomanNumeral(String s, int i) {
+    RomanNumeral(String s, int i) {
         this.i = i;
         this.s = s;
     }
@@ -16,5 +16,21 @@ public enum RomanNumeral {
 
     public String getS() {
         return s;
+    }
+
+    public static String toArabicNumeral(int a) {
+
+        int[] numbers = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] letters = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder roman = new StringBuilder();
+        int N = a;
+        for (int i = 0; i < numbers.length; i++) {
+            while (N >= numbers[i]) {
+                roman.append(letters[i]);
+                N -= numbers[i];
+            }
+        }
+        return roman.toString();
+
     }
 }
